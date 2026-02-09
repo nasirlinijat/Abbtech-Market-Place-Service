696c0f9c70f1d188b784398ce766b19759a3d2f8
javidanhatamov <cavidan.hatamov@gmail.com>
2026-02-09 22:01:01 +0400

    git

--- src/main/java/com/abbtech/Main.java
+++ src/main/java/com/abbtech/Main.java
@@ -11,6 +11,9 @@ public class Main {
 1. git clone -> clone repository to local
 2. git branch <branch name> -> create new branch
 3. git checkout <branch name> -> switch to new branch
-4.
+4. git add . -> add all files to staging area
+5. git commit -m "commit message" -> commit changes
+6. git push origin <branch name> -> push changes to remote
+7. git pull -> pull changes from remote
 
  */
