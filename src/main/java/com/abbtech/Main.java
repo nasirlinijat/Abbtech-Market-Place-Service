f1a89d02d123ae81778a2d3dba9ebb92f00878ff
javidanhatamov <cavidan.hatamov@gmail.com>
2026-02-09 21:05:44 +0400

    project initialized

---
+++ src/main/java/com/abbtech/Main.java
@@ -0,0 +1,16 @@
+package com.abbtech;
+
+public class Main {
+    static void main() {
+        System.out.println("Hello World!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Github!");
+    }
+}
+/*
+1. git clone -> clone repository to local
+2. git branch <branch name> -> create new branch
+3. git checkout <branch name> -> switch to new branch
+4.
+
+ */
