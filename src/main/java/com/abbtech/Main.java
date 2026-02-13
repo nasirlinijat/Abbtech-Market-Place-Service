fdb0d6ec81d8d2f71a87ec2b7786144e390ef4a8
javidanhatamov <cavidan.hatamov@gmail.com>
2026-02-13 09:53:17 +0400

    gradle init

--- src/main/java/com/abbtech/Main.java
+++ src/main/java/com/abbtech/Main.java
@@ -1,19 +1,17 @@
 package com.abbtech;
 
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        System.out.println("Hello World!");
-        System.out.println("Hello Git!");
-        System.out.println("Hello Github!");
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        IO.println(String.format("Hello and welcome!"));
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            IO.println("i = " + i);
+        }
     }
 }
-/*
-1. git clone -> clone repository to local
-2. git branch <branch name> -> create new branch
-3. git checkout <branch name> -> switch to new branch
-4. git add . -> add all files to staging area
-5. git commit -m "commit message" -> commit changes
-6. git push origin <branch name> -> push changes to remote
-7. git pull -> pull changes from remote
-
- */
