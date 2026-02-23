3861e7e35467da48cc051b812df0aaa1a9bd6cfe
javidanhatamov <cavidan.hatamov@gmail.com>
2026-02-23 22:01:53 +0400

    add jackson for better data response

--- src/main/java/com/abbtech/controller/ProductController.java
+++ src/main/java/com/abbtech/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.abbtech.controller;
 import com.abbtech.repository.ProductInMemoryRepositoryImpl;
 import com.abbtech.service.ProductService;
 import com.abbtech.service.ProductServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,6 +15,7 @@ import java.io.PrintWriter;
 @WebServlet(name = "ProductController", urlPatterns = "/products")
 public class ProductController extends HttpServlet {
     private ProductService productService;
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void init() {
@@ -22,6 +24,6 @@ public class ProductController extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter writer = response.getWriter();
-        writer.println(productService.getAllProducts());
+        writer.println(objectMapper.writeValueAsString(productService.getAllProducts()));
     }
 }
