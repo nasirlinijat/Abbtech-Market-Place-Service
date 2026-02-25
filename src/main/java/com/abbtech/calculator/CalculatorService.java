560a4702696c3bb6c9578898b1e6e6f69c1acd91
Nijat Nasirli <nicat.nasirli0511@gmail.com>
2026-02-25 20:33:12 +0400

    Created product and product data transfer object classes

--- src/main/java/com/abbtech/calculator/CalculatorService.java
+++
@@ -1,8 +0,0 @@
-package com.abbtech.calculator;
-
-public interface CalculatorService {
-    double add(double a, double b);
-    double subtract(double a, double b);
-    double multiply(double a, double b);
-    double divide(double a, double b);
-}
---
+++ src/main/java/com/abbtech/dto/ResponseProductDto.java
@@ -0,0 +1,50 @@
+package com.abbtech.dto;
+
+import java.math.BigDecimal;
+
+public class ResponseProductDto {
+    private String productName;
+    private BigDecimal productPrice;
+    private String productImage;
+    private String productDescription;
+
+    public ResponseProductDto(String productName, BigDecimal productPrice, String productImage, String productDescription) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productImage = productImage;
+        this.productDescription = productDescription;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public BigDecimal getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(BigDecimal productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(String productImage) {
+        this.productImage = productImage;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+}
