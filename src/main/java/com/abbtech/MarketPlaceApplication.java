9d352f39146a5aa81ac08bc2729bc16e0fbd5e1e
javidanhatamov <cavidan.hatamov@gmail.com>
2026-04-09 21:05:20 +0400

    Read configs from yaml file

--- src/main/java/com/abbtech/MarketPlaceApplication.java
+++ src/main/java/com/abbtech/MarketPlaceApplication.java
@@ -1,11 +1,18 @@
 package com.abbtech;
 
+import com.abbtech.profile.Employee;
+import com.abbtech.profile.EmployeeConfigurationProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MarketPlaceApplication {
     static void main(String[] args) {
-        SpringApplication.run(MarketPlaceApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(MarketPlaceApplication.class, args);
+        Employee employee = ctx.getBean("employeeValueAsBean", Employee.class);
+        EmployeeConfigurationProperty employeeConfigurationProperty = ctx.getBean(EmployeeConfigurationProperty.class);
+        System.out.println(employeeConfigurationProperty);
+        System.out.println(employee);
     }
 }
