4b22c6c45272046260376e66a3aa9cb71011bff9
javidanhatamov <cavidan.hatamov@gmail.com>
2026-04-25 11:59:10 +0400

    Validation with annotations and regular expressions

--- src/main/java/com/abbtech/dto/request/RequestBrandDto.java
+++ src/main/java/com/abbtech/dto/request/RequestBrandDto.java
@@ -1,16 +1,23 @@
 package com.abbtech.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
+import jdk.jfr.Registered;
 import lombok.Builder;
 import org.hibernate.validator.constraints.Length;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Builder
 public record RequestBrandDto(
         @NotBlank(message = "Brand name can not be empty or null") @Length(min = 2, max = 50) String name,
         String description,
         String image,
+        @AssertTrue
         Boolean isActive,
-        Boolean isDeleted) {
-
+        @AssertFalse
+        Boolean isDeleted, @Size(min = 1, max = 3) List<String> items,
+        @NotNull @Positive @Digits(integer = 1, fraction = 2) BigDecimal price,
+        @Pattern(regexp = "^\\+994(50|70)\\d{7}$") String mobileNumber) {
 }
 
